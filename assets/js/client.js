0e0f93fa92590494db45fc5eb4d053bb63280bf9
Matthew-Tangk <matthew.tangkilissan@gmail.com>
2023-06-19 18:08:59 +0200

    reset updates scripts

--- assets/js/client.js
+++ assets/js/client.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
         .querySelector(".status");
       if (toggle.checked) {
         statusElement.textContent = "Status: attending";
-        sendStatusToServer("attending"); // Call a function to send status to the server
+        console.log("attending");
       } else {
         statusElement.textContent = "Status: rejected";
-        sendStatusToServer("rejected"); // Call a function to send status to the server
+        console.log("rejected");
       }
     });
 
@@ -21,21 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
       .querySelector(".status");
     if (!toggle.checked) {
       statusElement.textContent = "Status: rejected";
-      sendStatusToServer("rejected"); // Call a function to send status to the server
     }
   });
-
-  // Function to send status to the server
-  function sendStatusToServer(status) {
-    fetch("/concertbuddies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ status }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }
 });
